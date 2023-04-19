0f07688c125c4f271de6abfabdc089d42af9a6bb
hjkim2040 <hjkim2661@gmail.com>
2023-04-19 15:24:53 +0900

    TestUt로 더 자세한 테스트

--- src/test/java/com/ll/exam1/MyArrayListTests.java
+++ src/test/java/com/ll/exam1/MyArrayListTests.java
@@ -1,5 +1,6 @@
 package com.ll.exam1;
 
+import com.ll.TestUt;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -32,7 +33,29 @@ class MyArrayListTests {
         list.add("사과");
         list.add("포도");
 
+        assertThat(list.get(0)).isEqualTo("사과");
         assertThat(list.get(1)).isEqualTo("포도");
+    }
+    @Test
+    @DisplayName("data(배열)의 크기가 자동으로 늘어나야 한다.")
+    void t4() {
+        MyArrayList<String> list = new MyArrayList<>();
+
+        String[] data = TestUt.getFieldValue(list, "data", null);
+        assertThat(data.length).isEqualTo(2);
+
+        list.add("사과");
+        list.add("포도");
+
+        data = TestUt.getFieldValue(list, "data", null);
+        assertThat(data.length).isEqualTo(2);
+
+        list.add("당근");
+
+        data = TestUt.getFieldValue(list, "data", null);
+        assertThat(data.length).isEqualTo(4);
+
+        assertThat(list.size()).isEqualTo(3);
     }
 
 }
